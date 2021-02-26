32b88143d2f8b54e4007947186c0367473fbd121
hemmy6894@gmail.com <hmanyinja@gmail.com>
2021-02-27 00:30:04 +0300

    first commit

---
+++ src/Controllers/RoleController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Hemmy\RoleManamger\Controllers;
+
+class RoleController {
+    public function __invoke($array = [])
+    {
+        print_r($array);
+    }
+}
---
+++ src/RoleManagerServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Hemmy\RoleManamger;
+
+use Illuminate\Support\ServiceProvider;
+
+class RoleManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Publishes configuration file.
+     *
+     * @return  void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/hemmy_role_manager.php' => config_path('hemmy_role_manager.php'),
+        ], 'laravel-log-enhancer-config');
+    }
+
+    /**
+     * Make config publishment optional by merging the config from the package.
+     *
+     * @return  void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/hemmy_role_manager.php',
+            'hemmy_role_manager'
+        );
+    }
+}
