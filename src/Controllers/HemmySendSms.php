291574efb9b414b182075d290c890fc2e0b7d902
hemmy6894@gmail.com <hmanyinja@gmail.com>
2021-04-28 15:24:30 +0300

    Update multiple SMS

--- src/Controllers/HemmySendSms.php
+++ src/Controllers/HemmySendSms.php
@@ -5,6 +5,7 @@ namespace Hemmy\SendSms\Controllers;
 class HemmySendSms
 {
     public static $api = "https://messaging-service.co.tz/api/sms/v1/text/single";
+    public static $apiMultiple = "https://messaging-service.co.tz/api/sms/v1/text/multi";
 
     /**
      * This is function to send sms only $to is required
@@ -17,14 +18,29 @@ class HemmySendSms
                 'text'  => $message
             ];
         $request = json_encode($array);
-        return HemmySendSms::request($request);
+        return HemmySendSms::request($request,HemmySendSms::$api);
     }
 
-    public static function request($array)
+    public static function sendMultiple($to=[],$message=[]){
+        $sender = config('hemmy_next_sms.sender')??"NEXTSMS";
+        $arrays = [];
+        foreach($to as $key => $val){
+            $array = [
+                    'from'  => $sender,
+                    'to'    => $val??null,
+                    'text'  => $message[$key]??null
+                ];
+            $arrays[] = $array;
+        }
+        $request = json_encode(["messages" => $arrays]);
+        return HemmySendSms::request($request,HemmySendSms::$apiMultiple);
+    }
+
+    public static function request($array,$api)
     {
         $headers = config('hemmy_next_sms.headers')??array();
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, HemmySendSms::$api);
+        curl_setopt($ch, CURLOPT_URL, $api);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $array);
